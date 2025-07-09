9bea75505b1cae3571939f0d41c86221f7a9844b
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2025-07-09 23:05:43 +0700

    fix(chat): refactor chat landing

--- src/pages/new/chat/page.tsx
+++ src/pages/new/chat/page.tsx
@@ -1,3 +1,4 @@
+import * as z from 'zod';
 import {
   ArrowRightIcon,
   ChatBubbleIcon,
@@ -6,22 +7,18 @@ import {
   PersonIcon,
   ReloadIcon
 } from '@radix-ui/react-icons';
-import { SetStateAction, useEffect, useRef, useState } from 'react';
-import useCreateChat from './_hook/use-create-chat';
-import useCreateNewChat from './_hook/use-create-new-chat';
-import { useGetDetailHistory } from './_hook/use-get-history-chat';
-import { ChatItem } from './component/ChatItem';
-import { Loader } from './component/Loader';
-import { useNavigate } from 'react-router-dom';
+import { useRef, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+
 import { useGetFiles } from '@/components/ui/sidebar/_hook/use-get-history-chat';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { Button } from '@/components/ui/button';
 
-type ChatResult = {
-  data?: {
-    answer?: string;
-    source_documents?: string[];
-  };
-};
+import useCreateChat from './_hook/use-create-chat';
+import useCreateNewChat from './_hook/use-create-new-chat';
+import { Loader } from './component/Loader';
 
 const promptSuggestions = [
   {
@@ -42,149 +39,166 @@ const promptSuggestions = [
   }
 ];
 
+const schema = z.object({
+  chat: z.string().min(1)
+});
+
+type ChatData = z.infer<typeof schema>;
+
 const ChatPage = () => {
-  const [text, setText] = useState('');
-  const [chat, setChat] = useState<ChatResult | null>(null);
+  const refButton = useRef<HTMLButtonElement | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
-  const [sessionId, setSessionId] = useState<string>();
-  const scrollAreaRef = useRef<HTMLDivElement>(null);
+
   const mutation = useCreateChat();
   const createNewChat = useCreateNewChat();
-  const navigate = useNavigate();
-  const currentPath = window.location.pathname;
-
-  const payload = {
-    question: text,
-    session_id: sessionId || ''
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value);
-  };
-  const query = useGetDetailHistory({ session_id: sessionId || '' });
   const queryHistorySideBar = useGetFiles();
-  const handleClickItem = (item: SetStateAction<string>) => {
-    setText(item);
+
+  const navigate = useNavigate();
+  const currentPath = useLocation().pathname;
+
+  const form = useForm<ChatData>({
+    mode: 'onChange',
+    resolver: zodResolver(schema)
+  });
+
+  const chatLength = form.watch('chat')?.length || 0;
+
+  const handleClickItem = (item: string) => {
+    form.setValue('chat', item);
   };
-  const handleClick = () => {
-    if (!text.trim()) return;
-    setChat({});
+
+  const handleSend = async (data: ChatData) => {
     setLoading(true);
-    mutation.mutate(payload, {
-      onSuccess: (data) => {
+    try {
+      const sessionResponse = await createNewChat.mutateAsync();
+
+      const sessionId = sessionResponse?.data?.session_id;
+      if (!sessionId) {
         setLoading(false);
-        setChat({
-          data: data?.data ?? undefined
-        });
-        setText('');
-        query.refetch();
-        queryHistorySideBar.refetch();
-        navigate(`${currentPath}/${sessionId}`);
-      },
-      onError: () => {
-        setLoading(false);
+        return;
       }
-    });
-  };
-  useEffect(() => {
-    if (!sessionId) {
-      createNewChat.mutate(undefined, {
-        onSuccess: (data) => {
-          setSessionId(data.data.session_id);
+
+      const trimmedQuestion = data.chat.trim();
+
+      mutation.mutate(
+        {
+          session_id: sessionId,
+          question: trimmedQuestion
         },
-        onError: (error) => {
-          console.error('error', error);
+        {
+          onSuccess: () => {
+            queryHistorySideBar.refetch();
+            form.reset();
+            navigate(`${currentPath}/${sessionId}`);
+          },
+          onError: (err) => {
+            console.error('Chat mutation failed', err);
+          },
+          onSettled: () => {
+            setLoading(false);
+          }
         }
-      });
+      );
+    } catch (error) {
+      console.error('Session creation failed', error);
+      setLoading(false);
     }
-  }, []);
+  };
 
   return (
-    <div className="mx-auto flex w-full flex-1 flex-col items-center justify-center text-left md:max-w-4xl">
-      {loading ? (
-        <Loader />
-      ) : chat && !loading ? (
-        <ScrollArea className="scrollbar-hide flex-1" ref={scrollAreaRef}>
-          <div className="min-h-full">
-            {query?.data?.data?.map((message, index) => (
-              <ChatItem key={index} data={message} />
-            ))}
-          </div>
-        </ScrollArea>
-      ) : (
-        <div className="w-full">
-          <h2 className="text-gradient-light text-2xl font-bold md:text-4xl">
-            Hi there, Marvin
-          </h2>
-          <h3 className="text-gradient-light mt-1 text-2xl font-bold md:mt-2 md:text-4xl">
-            What would you like to know?
-          </h3>
-          <p className="mt-2 text-gray-500 md:mt-6">
-            Use one of the most common prompts below or use your own to begin
-          </p>
+    <div className="flex w-full flex-1 flex-col items-center justify-center text-left">
+      <form
+        className="mx-auto w-full md:max-w-4xl"
+        onSubmit={form.handleSubmit(handleSend)}
+      >
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="w-full">
+            <h2 className="text-gradient-light text-2xl font-bold md:text-3xl lg:text-4xl">
+              Hi there, Marvin
+            </h2>
+            <h3 className="text-gradient-light mt-1 text-2xl font-bold md:mt-2 md:text-3xl lg:text-4xl">
+              What would you like to know?
+            </h3>
+            <p className="mt-2 text-gray-500 md:mt-6">
+              Use one of the most common prompts below or use your own to begin
+            </p>
 
-          <ScrollArea className="mt-2 w-full overflow-x-auto md:mt-8">
-            <div className="flex w-max gap-4 py-2">
-              {promptSuggestions.map((prompt, index) => (
-                <div
-                  key={index}
-                  onClick={() => handleClickItem(prompt.text)}
-                  className="flex min-h-20 w-52 shrink-0 cursor-pointer flex-col items-start rounded-md border border-gray-200 p-2 hover:bg-gray-50 md:rounded-lg md:p-4"
-                >
-                  <p className="flex-1 text-xs text-gray-700 md:text-sm">
-                    {prompt.text}
-                  </p>
-                  {prompt.icon}
+            <ScrollArea className="mt-2 w-full overflow-x-auto md:mt-8">
+              <div className="flex w-max gap-4 py-2">
+                {promptSuggestions.map((prompt, index) => (
+                  <div
+                    key={index}
+                    onClick={() => handleClickItem(prompt.text)}
+                    className="flex min-h-20 w-52 shrink-0 cursor-pointer flex-col items-start rounded-md border border-gray-200 p-2 hover:bg-gray-50 md:rounded-lg md:p-4"
+                  >
+                    <p className="flex-1 text-xs text-gray-700 md:text-sm">
+                      {prompt.text}
+                    </p>
+                    {prompt.icon}
+                  </div>
+                ))}
+              </div>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+
+            <Button variant="ghost" className="mt-2 flex text-sm md:mt-3">
+              <ReloadIcon className="mr-2" />
+              Refresh prompts
+            </Button>
+          </div>
+        )}
+
+        <Controller
+          control={form.control}
+          name="chat"
+          render={({ field }) => (
+            <div className="mt-2 w-full rounded-xl border border-gray-300 bg-white p-3 md:mt-4 md:p-4">
+              <textarea
+                className="w-full resize-none border-none text-sm outline-none placeholder:text-gray-400"
+                rows={3}
+                placeholder="Ask CombipharGPT whatever you want....."
+                maxLength={1000}
+                value={field.value}
+                onChange={(e) => field.onChange(e.target.value.trimStart())}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' && !e.shiftKey) {
+                    e.preventDefault();
+                    refButton.current?.click();
+                  }
+                }}
+              />
+
+              <div className="mt-4 flex items-center justify-between text-sm text-gray-800">
+                <div className="flex items-center gap-6">
+                  <button className="flex items-center gap-1 transition hover:text-purple-600">
+                    {/* <PlusCircledIcon /> Add attachment */}
+                  </button>
+                  <button className="flex items-center gap-1 transition hover:text-purple-600">
+                    {/* <ImageIcon /> Use image */}
+                  </button>
                 </div>
-              ))}
+
+                <div className="flex items-center gap-3">
+                  <span className="text-sm text-gray-900">
+                    {chatLength}/1000
+                  </span>
+                  <Button
+                    ref={refButton}
+                    size="icon"
+                    type="submit"
+                    disabled={!form.formState.isValid || loading}
+                    className="flex h-8 w-8 items-center justify-center rounded-md bg-[#7051f8] text-white transition hover:bg-[#5b3de4]"
+                  >
+                    <ArrowRightIcon />
+                  </Button>
+                </div>
+              </div>
             </div>
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
-          <button className="mt-2 flex text-sm text-gray-600 hover:text-gray-900 md:mt-6">
-            <ReloadIcon className="mr-2" />
-            Refresh prompts
-          </button>
-        </div>
-      )}
-
-      <div className="mt-2 w-full rounded-xl border border-gray-300 bg-white p-3 md:mt-4 md:p-4">
-        <textarea
-          className="w-full resize-none border-none text-sm outline-none placeholder:text-gray-400"
-          rows={3}
-          placeholder="Ask CombipharGPT whatever you want....."
-          maxLength={1000}
-          value={text}
-          onChange={handleChange}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter' && !e.shiftKey) {
-              e.preventDefault();
-              setText('');
-              handleClick();
-            }
-          }}
+          )}
         />
-
-        <div className="mt-4 flex items-center justify-between text-sm text-gray-800">
-          <div className="flex items-center gap-6">
-            <button className="flex items-center gap-1 transition hover:text-purple-600">
-              {/* <PlusCircledIcon /> Add attachment */}
-            </button>
-            <button className="flex items-center gap-1 transition hover:text-purple-600">
-              {/* <ImageIcon /> Use image */}
-            </button>
-          </div>
-
-          <div className="flex items-center gap-3">
-            <span className="text-sm text-gray-900">{text.length}/1000</span>
-            <button
-              className="flex h-8 w-8 items-center justify-center rounded-md bg-[#7051f8] text-white transition hover:bg-[#5b3de4]"
-              onClick={handleClick}
-            >
-              <ArrowRightIcon />
-            </button>
-          </div>
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
