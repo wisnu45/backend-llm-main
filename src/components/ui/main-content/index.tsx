2cec51e5d0223250d8f273f2255c460a38a37dbc
tj <teguhjunian01@gmail.com>
2025-08-12 15:38:04 +0700

    fix : change ukuran font

--- src/components/ui/main-content/index.tsx
+++ src/components/ui/main-content/index.tsx
@@ -4,9 +4,7 @@ const MainContent = () => {
   return (
     <main className="flex h-full flex-1 flex-col overflow-y-auto p-6">
       <header className="mb-10 flex items-center justify-between">
-        <h3 className="mb-1 text-2xl font-bold md:text-3xl lg:text-4xl">
-          VITA
-        </h3>
+        <h3 className="text-1xl mb-1 font-bold">VITA</h3>
       </header>
       <Outlet />
     </main>
