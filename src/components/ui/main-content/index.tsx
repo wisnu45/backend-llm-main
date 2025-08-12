be909df033c352437af523250cb43eb2f57e584a
tj <teguhjunian01@gmail.com>
2025-08-12 11:28:38 +0700

    change color vita

--- src/components/ui/main-content/index.tsx
+++ src/components/ui/main-content/index.tsx
@@ -4,7 +4,7 @@ const MainContent = () => {
   return (
     <main className="flex h-full flex-1 flex-col overflow-y-auto p-6">
       <header className="mb-10 flex items-center justify-between">
-        <h3 className="text-gradient-light mb-1 text-2xl font-bold md:text-3xl lg:text-4xl">
+        <h3 className="mb-1 text-2xl font-bold md:text-3xl lg:text-4xl">
           VITA
         </h3>
       </header>
