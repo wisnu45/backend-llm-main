07b1dab565f48d623e6ab7d2ea3ba269683e3516
tj <teguhjunian01@gmail.com>
2025-08-13 18:56:25 +0700

    change typografi

--- src/components/ui/main-content/index.tsx
+++ src/components/ui/main-content/index.tsx
@@ -4,7 +4,7 @@ const MainContent = () => {
   return (
     <main className="flex h-full flex-1 flex-col overflow-y-auto p-4">
       <header className="mb-5 flex items-center justify-between">
-        <h3 className="mb-1 text-2xl font-bold">VITA</h3>
+        <h3 className="mb-1 text-2xl">Vita</h3>
       </header>
       <Outlet />
     </main>
