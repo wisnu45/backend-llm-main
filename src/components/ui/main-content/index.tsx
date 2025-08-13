aa4f51f84055d2886e03806d7ea7cbd78fd55742
tj <teguhjunian01@gmail.com>
2025-08-13 18:41:13 +0700

    change layout main content

--- src/components/ui/main-content/index.tsx
+++ src/components/ui/main-content/index.tsx
@@ -2,9 +2,9 @@ import { Outlet } from 'react-router-dom';
 
 const MainContent = () => {
   return (
-    <main className="flex h-full flex-1 flex-col overflow-y-auto p-6">
-      <header className="mb-10 flex items-center justify-between">
-        <h3 className="text-1xl mb-1 font-bold">VITA</h3>
+    <main className="flex h-full flex-1 flex-col overflow-y-auto p-4">
+      <header className="mb-5 flex items-center justify-between">
+        <h3 className="mb-1 text-2xl font-bold">VITA</h3>
       </header>
       <Outlet />
     </main>
