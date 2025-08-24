77e187fbc2eee2c9ea5ffd03c068b2bcd87beced
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2025-08-24 21:42:56 +0700

    fix: text wrapping on file references

--- src/pages/new/chat/component/chat-item/FileReferences.tsx
+++ src/pages/new/chat/component/chat-item/FileReferences.tsx
@@ -12,7 +12,7 @@ export const FileReferences = ({ fileLinks }: { fileLinks: FileLink[] }) => {
     <div className="w-full">
       <hr className="mb-2 w-full border-t-4 border-[#C4C4C480]" />
       <div className="font-bold">Referensi Sumber:</div>
-      <div className="flex flex-col">
+      <div className="flex flex-col space-y-1">
         {fileLinks.map((item, index) => (
           <a
             key={item.download_url}
@@ -21,7 +21,7 @@ export const FileReferences = ({ fileLinks }: { fileLinks: FileLink[] }) => {
               e.preventDefault();
               openPdf.mutate(item.download_url);
             }}
-            className="inline-block w-max cursor-pointer rounded-md px-2 py-1 text-blue-400 hover:underline"
+            className="overflow-wrap-anywhere inline-block max-w-full cursor-pointer break-words rounded-md px-2 py-1 text-blue-400 hover:underline"
           >
             {index + 1}. {item.filename}
           </a>
