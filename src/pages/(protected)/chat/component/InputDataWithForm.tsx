b7cedf72516e8c85c2977d57799cf4cf3bd3b018
tj <teguhjunian01@gmail.com>
2025-10-17 14:11:28 +0700

    fix input data with form

--- src/pages/(protected)/chat/component/InputDataWithForm.tsx
+++ src/pages/(protected)/chat/component/InputDataWithForm.tsx
@@ -219,16 +219,16 @@ const InputDataWithForm = ({
     setPopupFile(null);
   };
 
-  const convertFileToBase64 = (file: File): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        resolve(reader.result as string);
-      };
-      reader.onerror = (error) => reject(error);
-      reader.readAsDataURL(file);
-    });
-  };
+  // const convertFileToBase64 = (file: File): Promise<string> => {
+  //   return new Promise((resolve, reject) => {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       resolve(reader.result as string);
+  //     };
+  //     reader.onerror = (error) => reject(error);
+  //     reader.readAsDataURL(file);
+  //   });
+  // };
 
   const validateFile = (file: File) => {
     const fileExtension = file.name.split('.').pop()?.toLowerCase();
