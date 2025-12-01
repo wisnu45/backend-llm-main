0ce5b09e5f3aef79a320a3695bc8b3ab764a775a
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2025-12-01 20:19:40 +0700

    fix: responsive layout

--- src/components/providers/main-layout.tsx
+++ src/components/providers/main-layout.tsx
@@ -5,16 +5,16 @@ import Logout from '@/components/ui/Logout';
 const MainLayout = ({ children }: PropsWithChildren) => {
   const [showModal, setShowModal] = useState(false);
   return (
-    <div className="flex h-screen bg-[#EEEEEE] font-sans text-[#1B212D]">
+    <div className="h-screen bg-[#EEEEEE] font-sans text-[#1B212D]">
       <div className="fixed inset-y-0 left-0 z-20">
         <Sidebar setShowModal={setShowModal} />
       </div>
       <div
-        className="relative flex h-full flex-1 flex-col"
+        className="relative flex h-full flex-col"
         style={{ marginLeft: 'var(--sidebar-width, 50px)' }}
       >
         <header
-          style={{ marginLeft: 'var(--sidebar-width, 50px)' }}
+          style={{ left: 'var(--sidebar-width, 50px)' }}
           className="fixed inset-x-0 top-0 z-10 flex h-16 items-center justify-between bg-[#EEEEEE] px-4"
         >
           <h3 className="text-2xl">Vita</h3>
